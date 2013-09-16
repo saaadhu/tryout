32cd5b28e94eebc813a527cd521b02544946c365
Senthil Kumar Selvaraj <senthil.thecoder@gmail.com>
2013-09-16 18:31:40 +0530

    Show build in a reddish color if compilation fails

--- src/tryout/www/scripts/tryout.js
+++ src/tryout/www/scripts/tryout.js
@@ -2,12 +2,17 @@ var app = angular.module('tryout', ['ui.bootstrap']);
 
 function TryoutCtrl ($http, $scope) {
     $scope.options = '';
+    $scope.status_color = '#FFFFFF';
 
     $scope.compile = function() {
+        $scope.output = '';
+        $scope.lss = '';
+        $scope.status_color = '#FFFFFF';
         $http.post ("/compile",
                 { "code" : $scope.code, "options" : $scope.options }).success (function (response) {
                   $scope.output = response.BuildOutput;
                   $scope.lss = response.Listing;
+                  $scope.status_color = response.Success ? "#FFFFFF" : '#FFCCCC';
                 });
     };
 }
